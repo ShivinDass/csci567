c2e9cd4b83bcdcf5c52ffc707ef47066bbe20080
ShivinDass <shivindass@gmail.com>
2022-04-18 18:55:34 -0700

    choose most frequent

--- csci567/utils/data_utils.py
+++ csci567/utils/data_utils.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-def load_csv(filename):
+def load_csv(filename, dtype=str):
 	'''
 		load file from DATA_DIR/filename
 	'''
@@ -10,13 +10,23 @@ def load_csv(filename):
 	path = os.path.join(data_path, filename)
 	
 	if os.path.exists(path):
-		return pd.read_csv(path)
+		return pd.read_csv(path, dtype=dtype)
 	else:
 		raise Exception(
 				"Incorrect filepath: " + path
 			)
 
-def get_train_data():
+def save_csv(data, filename):
+	'''
+		save data to DAT_DIR/filename
+	'''
+
+	data_path = os.environ["DATA_DIR"]
+	path = os.path.join(data_path, filename)
+
+	data.to_csv(path, index = False)
+
+def get_train_data(cutoff_date = '2020-09-15'):
 	'''
 		generate training data by only returning the transactions that took place on or before 2020-09-15
 	'''
@@ -24,28 +34,31 @@ def get_train_data():
 	train = load_csv("transactions_train.csv")
 
 	train.t_dat = pd.to_datetime(train.t_dat)
-	return train.loc[train.t_dat <= pd.to_datetime('2020-09-15')]
+	if cutoff_date:
+		return train.loc[train.t_dat <= pd.to_datetime(cutoff_date)]
+	return train
 
-def get_val_data():
+def get_val_data(cutoff_date = '2020-09-16'):
 	'''
 		generate validation data by only returning the articles purchased in the week 2020-09-16 to 2020-09-22
 	'''
 
-	path = os.path.join(os.environ["DATA_DIR"], "validation.csv")
+	path = os.path.join(os.environ["DATA_DIR"], "validation_{}.csv".format(cutoff_date))
 	
 	if not os.path.exists(path):
 		val = load_csv("transactions_train.csv")
 		
 		val.t_dat = pd.to_datetime(val.t_dat)
-		val = val.loc[val.t_dat >= pd.to_datetime('2020-09-16')]
+		val = val.loc[val.t_dat >= pd.to_datetime(cutoff_date)]
 
 		val = val.groupby('customer_id').article_id.apply(list).reset_index()
 		val = val.rename({'article_id' : 'prediction'}, axis = 1)
 
-		val['prediction'] = val.prediction.apply(lambda x: ' '.join(['0'+str(k) for k in x]))
+		val['prediction'] = val.prediction.apply(lambda x: ' '.join([str(k) for k in x]))
 		
-		val.to_csv(path)		
+		val.rename(columns={"Unnamed: 0": "customer_id"})
+		val.to_csv(path)
 
-	val = load_csv("validation.csv")
+	val = load_csv("validation_{}.csv".format(cutoff_date))
 
 	return val
---
+++ csci567/utils/utils.py
@@ -0,0 +1,14 @@
+from csci567.metrics.mapk import mapk
+from csci567.utils.data_utils import load_csv
+
+def get_val_score(val_file = "validation.csv", sub_file = "submission.csv"):
+	'''
+		Compute the validation score between val_file and sub_file
+
+	'''
+	val = load_csv(val_file)
+
+	sub = load_csv(sub_file)
+	sub.set_index('customer_id').loc[val.customer_id].reset_index()
+
+	return mapk(val.prediction.str.split(), sub.prediction.str.split(), k=12)
