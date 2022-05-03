4c8298ab2eab5934a043f3261583abbef7b773b6
jullian-yapeter <jullianyapeter@gmail.com>
2022-05-03 00:17:55 -0700

    Add validation feature

--- csci567/models/two_tower_model.py
+++ csci567/models/two_tower_model.py
@@ -36,16 +36,12 @@ def get_customer_purchases(transactions_df, after="2020-08-23"):
         customers_index_dict, \
         articles_index_dict
 
-def predict(queries, objects, two_tower_model):
-    '''return csv of queries and list of predicted objects'''
-    # Run all queries and objects through two_tower_model
-    # Take argmax of logits
-    pass
-
 
 class PurchasesDataset(Dataset):
     '''dataset of purchases for contrastive learning'''
-    def __init__(self, purchases_df, customers_index_dict, articles_index_dict):
+    def __init__(self, purchases_df, customers_index_dict, articles_index_dict, device=None):
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu') if device is None else device
         self.customer_ids = purchases_df["customer_id"].values
         self.purchases_ids = purchases_df["article_id"].values
         self.customers_index_dict = customers_index_dict
@@ -55,13 +51,13 @@ class PurchasesDataset(Dataset):
         return len(self.customer_ids)
 
     def __getitem__(self, idx):
-        return self.customers_index_dict[self.customer_ids[idx]], \
-            self.articles_index_dict[np.random.choice(self.purchases_ids[idx])]
+        return torch.tensor(self.customers_index_dict[self.customer_ids[idx]]).to(self.device), \
+            torch.tensor(self.articles_index_dict[np.random.choice(self.purchases_ids[idx])]).to(self.device)
 
 
 class Encoder(nn.Module):
     '''Implements encoder to translate input embeddings into a meaningful representation'''
-    def __init__(self, in_dim, out_dim, num_hidden_layers=3, hidden_layer_dim=32, activation=nn.LeakyReLU, device=None):
+    def __init__(self, in_dim, out_dim, num_hidden_layers=0, hidden_layer_dim=12, activation=nn.LeakyReLU, device=None):
         super().__init__()
         self.device = torch.device(
             'cuda' if torch.cuda.is_available() else 'cpu') if device is None else device
@@ -81,7 +77,7 @@ class Encoder(nn.Module):
 class TwoTowerModel(nn.Module):
     '''Implements two tower contrastive learning'''
     def __init__(self, num_queries, num_objects,
-                 input_embed_dim=40, representation_embed_dim=32, device=None):
+                 input_embed_dim=12, representation_embed_dim=12, device=None):
         super().__init__()
         self.device = torch.device(
             'cuda' if torch.cuda.is_available() else 'cpu') if device is None else device
@@ -93,15 +89,26 @@ class TwoTowerModel(nn.Module):
             input_embed_dim, representation_embed_dim, device=device).to(self.device)
         self.object_net = Encoder(
             input_embed_dim, representation_embed_dim, device=device).to(self.device)
-        self.loss_criterion = nn.CrossEntropyLoss()
+        self.loss_criterion = nn.CrossEntropyLoss(reduction="mean")
 
     def forward(self, contrastive_batch):
         queries, objects = contrastive_batch
-        queries_latents = self.query_net(self.queries_embeddings(queries))
-        objects_latents = self.object_net(self.objects_embeddings(objects))
-        target_perm = torch.randperm(len(objects_latents))
-        objects_latents = objects_latents[target_perm]
+        target_perm = torch.randperm(len(objects), requires_grad=False).to(self.device)
+        # queries_latents = self.query_net(self.queries_embeddings(queries))
+        # objects_latents = self.object_net(self.objects_embeddings(objects))
+        queries_latents = self.queries_embeddings(queries)
+        objects_latents = self.objects_embeddings(objects[target_perm])
         return torch.matmul(queries_latents, objects_latents.T), target_perm
+
+    def predict(self, test_batch):
+        with torch.no_grad():
+            queries, objects = test_batch
+            # queries_latents = self.query_net(self.queries_embeddings(queries))
+            # objects_latents = self.object_net(self.objects_embeddings(objects))
+            queries_latents = self.queries_embeddings(queries)
+            objects_latents = self.objects_embeddings(objects)
+            logits = torch.matmul(queries_latents, objects_latents.T)
+        return torch.argmax(logits, dim=1)
 
     def loss(self, logits, targets):
         return self.loss_criterion(logits, targets)
@@ -109,13 +116,14 @@ class TwoTowerModel(nn.Module):
 
 class TwoTowerTrainer:
     '''training loop of two tower model'''
-    def __init__(self, model, dataloader, epochs, learning_rate, experiment_name="two_tower"):
+    def __init__(self, model, train_dataloader, test_dataloader, epochs, learning_rate, experiment_name="two_tower"):
         self.experiment_name = experiment_name
         self.save_model_path = os.path.join(
             os.environ["EXP_DIR"], f"{experiment_name}.pt")
         self.save_plot_path = os.path.join(
             os.environ["EXP_DIR"], f"{experiment_name}_plot.jpg")
-        self.dataloader = dataloader
+        self.train_dataloader = train_dataloader
+        self.test_dataloader = test_dataloader
         self.epochs = epochs
         self.lr = learning_rate
         self.model = model.train()
@@ -125,10 +133,10 @@ class TwoTowerTrainer:
     def train(self):
         train_it = 0
         best_loss = float('inf')
-        losses = []
+        losses, val_accs = [], []
         for epoch in tqdm(range(self.epochs)):
             print(f"Run epoch {epoch}")
-            for batch in tqdm(self.dataloader):
+            for batch in tqdm(self.train_dataloader):
                 self.opt.zero_grad()
                 logits, targets = self.model(batch)
                 loss = self.model.loss(logits, targets)
@@ -140,38 +148,63 @@ class TwoTowerTrainer:
                     print(f"saving checkpoint. new best loss: {new_loss}")
                     torch.save(self.model, self.save_model_path)
                     best_loss = new_loss
-                if train_it % 1000 == 0:
+                if train_it % 500 == 0:
                     print(f"It {train_it}: Total Loss: {loss.cpu().detach()}")
+                    val_acc = self.validate()
+                    print(f"It {train_it}: Val Acc: {val_acc}")
+                    val_accs.append(val_acc)
                 train_it += 1
             # log the loss training curves
             plt.figure(figsize=(15, 5))
-            ax1 = plt.subplot(111)
+            ax1 = plt.subplot(121)
             ax1.plot(losses)
             ax1.title.set_text("loss")
+            ax1 = plt.subplot(122)
+            ax1.plot(val_accs)
+            ax1.title.set_text("val acc")
             plt.savefig(self.save_plot_path)
         print("Done!")
+
+    def validate(self):
+        test_batch = None
+        correct = 0
+        for batch in self.test_dataloader:
+            test_batch = batch
+        prediction = self.model.predict(test_batch)
+        for i in range(len(prediction)):
+            correct += 1 if prediction[i] == i else 0
+        return correct / len(prediction)
+
 
 
 if __name__ == "__main__":
     # Init
-    experiment_name = "two_tower"
+    experiment_name = "two_tower_all_no_nn"
     print(f"Initializing experiment: {experiment_name}")
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    epochs = 10
-    learning_rate = 1e-4
+    epochs = 10000
+    val_size = 100
+    learning_rate = 2e-3
 
     # Load in training data
     print("Loading in training data")
     transactions_df = get_train_data(cutoff_date=None)
     purchases_df, customers_index_dict, articles_index_dict = get_customer_purchases(
         transactions_df, after="2020-08-23")
+    training_purchases_df = purchases_df
+    testing_purchases_df = purchases_df[:val_size]
+    del transactions_df
 
     # Make dataset and dataloader
     print("Making purchases dataloader")
-    purchases_dataset = PurchasesDataset(
-        purchases_df, customers_index_dict, articles_index_dict)
-    purchases_dataloader = DataLoader(
-        purchases_dataset, batch_size=32, shuffle=True)
+    training_purchases_dataset = PurchasesDataset(
+        training_purchases_df, customers_index_dict, articles_index_dict, device=device)
+    training_purchases_dataloader = DataLoader(
+        training_purchases_dataset, batch_size=64, shuffle=True)
+    testing_purchases_dataset = PurchasesDataset(
+        testing_purchases_df, customers_index_dict, articles_index_dict, device=device)
+    testing_purchases_dataloader = DataLoader(
+        testing_purchases_dataset, batch_size=val_size, shuffle=True)
 
     # Make model and trainer
     print("Making model and trainer")
@@ -180,7 +213,7 @@ if __name__ == "__main__":
     count_parameters(two_tower_model)
 
     two_tower_trainer = TwoTowerTrainer(
-        two_tower_model, purchases_dataloader, epochs, learning_rate, experiment_name)
+        two_tower_model, training_purchases_dataloader, testing_purchases_dataloader, epochs, learning_rate, experiment_name)
     
     # Start training
     print("Starting training")
